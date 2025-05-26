b7aaab91702d3160ed449e7b5581c5f8293b6691
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-05-25 23:33:23 -0400

    second commit

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,42 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 export default function Header() {
-  return <div>header here ..</div>;
+  return (
+    <section className="flex items-center justify-between w-full h-20 px-8 bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+      <div className="flex items-center">
+        <Link href="/">
+          <Image src="/logo.svg" width={60} height={60} alt="Logo" priority />
+        </Link>
+
+        <nav className="ml-10 space-x-4">
+          <Link
+            href="/about"
+            className="text-gray-900 dark:text-white hover:underline"
+          >
+            About
+          </Link>
+          <Link
+            href="/contact"
+            className="text-gray-900 dark:text-white hover:underline"
+          >
+            Contact
+          </Link>
+          <Link
+            href="/services"
+            className="text-gray-900 dark:text-white hover:underline"
+          >
+            Services
+          </Link>
+          <Link
+            href="/blog"
+            className="text-gray-900 dark:text-white hover:underline"
+          >
+            Blog
+          </Link>
+        </nav>
+      </div>
+    </section>
+  );
 }
