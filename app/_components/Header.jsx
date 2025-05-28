4cbc87d102d63fbd2b5294baaae397056a9e4ee5
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-05-28 05:54:43 -0400

    updated header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,41 +1,40 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
   return (
-    <section className="flex items-center justify-between w-full h-20 px-8 bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <section className="flex items-between justify-between w-full h-20 px-8 bg-white shadow-sm">
       <div className="flex items-center">
         <Link href="/">
-          <Image src="/logo.svg" width={60} height={60} alt="Logo" priority />
+          <Image src="/logo.svg" width={150} height={60} alt="Logo" priority />
         </Link>
 
-        <nav className="ml-10 space-x-4">
-          <Link
-            href="/about"
-            className="text-gray-900 dark:text-white hover:underline"
-          >
-            About
+        <nav className="hidden md:flex ml-10 space-x-4">
+          <Link href="/about" className="text-gray-900 hover:underline">
+            For Sale
           </Link>
-          <Link
-            href="/contact"
-            className="text-gray-900 dark:text-white hover:underline"
-          >
-            Contact
+          <Link href="/contact" className="text-gray-900 hover:underline">
+            For Rent
           </Link>
-          <Link
-            href="/services"
-            className="text-gray-900 dark:text-white hover:underline"
-          >
-            Services
-          </Link>
-          <Link
-            href="/blog"
-            className="text-gray-900 dark:text-white hover:underline"
-          >
-            Blog
+          <Link href="/services" className="text-gray-900 hover:underline">
+            Agent Finder
           </Link>
         </nav>
+      </div>
+
+      <div className="right-div flex items-center ml-auto space-x-4">
+        <Button asChild className="bg-brand hover:bg-brand-dark text-white">
+          <Link href="/post-ad">
+            <Plus /> Post Your Ad
+          </Link>
+        </Button>
+
+        <Button asChild variant="outline">
+          <Link href="/login">Login</Link>
+        </Button>
       </div>
     </section>
   );
