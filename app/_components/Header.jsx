9616726aaeaa5cf958bfa20eed4265636eb1c328
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-05-31 18:00:26 -0400

    updated header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -6,20 +6,36 @@ import { Button } from "@/components/ui/button";
 
 export default function Header() {
   return (
-    <section className="flex items-between font-text justify-between w-full h-20 px-8 bg-white shadow-sm">
+    <section className="flex items-between font-text justify-between w-full h-20 px-8 bg-white shadow-sm fixed top-0 z-50">
       <div className="flex items-center">
         <Link href="/">
-          <Image src="/logo.svg" width={150} height={60} alt="Logo" priority />
+          <Image
+            src="/logo.svg"
+            width={150}
+            height={60}
+            alt="Logo"
+            style={{ height: "120px", width: "120px" }}
+            priority
+          />
         </Link>
 
         <nav className="hidden md:flex ml-10 space-x-4">
-          <Link href="/about" className="text-brand hover:underline">
+          <Link
+            href="/about"
+            className="text-gray-900 hover:text-[var(--brand)] text-sm"
+          >
             For Sale
           </Link>
-          <Link href="/contact" className="text-gray-900 hover:underline">
+          <Link
+            href="/contact"
+            className="text-gray-900 hover:text-[var(--brand)] text-sm"
+          >
             For Rent
           </Link>
-          <Link href="/services" className="text-gray-900 hover:underline">
+          <Link
+            href="/services"
+            className="text-gray-900 hover:text-[var(--brand)] text-sm"
+          >
             Agent Finder
           </Link>
         </nav>
