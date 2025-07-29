7e3b505eaa365188a886b86a6bfbc391e2f1c573
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-07-29 16:06:26 -0400

    working on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
 import { Button } from "../components/ui/button";
 import Link from "next/link";
+import Listing from "./_components/Listing";
+import ListingMapView from "./_components/ListingMapView";
+import { supabase } from "../utils/supabase/client";
 
 export default function Home() {
+  async function getListing() {
+    const { data, error } = await supabase
+      .from("listing")
+      .select("*, listingImages(url, listing_id)");
+  }
   return (
-    <div className="flex">
-      <div className="content"></div>
-      <div className="map"></div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5">
+      <div className="content">
+        <Listing />
+      </div>
+      <div className="map">
+        <ListingMapView />
+      </div>
     </div>
   );
 }
