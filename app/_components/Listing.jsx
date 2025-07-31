f4f3d7edad121a8de4bdadc49a75ae3d78a075a8
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-07-31 16:01:44 -0400

    working on listing component

--- app/_components/Listing.jsx
+++ app/_components/Listing.jsx
@@ -2,11 +2,10 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import PriceFormat from "../../lib/PriceFormat";
 import { supabase } from "../../utils/supabase/client";
 import { toast } from "sonner";
 import { Button } from "../../@/components/ui/button";
-
 export default function Listing({ type }) {
   const [listings, setListings] = useState([]);
 
@@ -34,24 +33,26 @@ export default function Listing({ type }) {
         <div className="listings grid grid-cols-1 md:grid-cols-2 gap-5">
           {listings.length > 0
             ? listings.map((listing) => (
-                <div key={listing.id} className="listing-item">
-                  <Image
-                    src={
-                      listing.listingImages[0]?.url ||
-                      "/media/placeholder-image.svg"
-                    }
-                    alt={`Apartment For Rent at: ${listing.address}`}
-                    width={300}
-                    height={200}
-                    className="object-cover w-[100%] h-[300px] rounded-lg"
-                  />
-                  <h2>{listing.address}</h2>
-                  <p>{listing.description}</p>
-                  <p>Price: ${listing.price}</p>
-                  <Link href={`/view-listing/${listing.id}`}>
-                    <Button>View Details</Button>
-                  </Link>
-                </div>
+                <Link
+                  className="hover:opacity-90"
+                  href={`/view-listing/${listing.id}`}
+                >
+                  <div key={listing.id} className="listing-item">
+                    <Image
+                      src={
+                        listing.listingImages[0]?.url ||
+                        "/media/placeholder-image.svg"
+                      }
+                      alt={`Apartment For Rent at: ${listing.address}`}
+                      width={300}
+                      height={200}
+                      className="object-cover w-[100%] h-[300px] rounded-lg"
+                    />
+                    <h2>{listing.address}</h2>
+                    <p>{listing.description}</p>
+                    <p>${PriceFormat(listing.price)}</p>
+                  </div>
+                </Link>
               ))
             : [1, 2, 3, 4].map((item, index) => (
                 <div
