b22e93a90a197e97b6810742b813bc3869843a7e
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-08-19 09:58:24 -0400

    updated user page

--- app/dashboard/[[...rest]]/page.jsx
+++ app/dashboard/[[...rest]]/page.jsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useEffect } from "react";
-import { useUser, UserProfile } from "@clerk/nextjs";
+import { useUser, UserProfile, UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
+import { Building } from "lucide-react";
 
 export default function DashboardPage() {
   const { user, isLoaded } = useUser();
@@ -17,5 +18,19 @@ export default function DashboardPage() {
   if (!isLoaded) return <p>Loading...</p>;
   if (!user) return <p>Access denied.</p>;
 
-  return <UserProfile />;
+  return (
+    <div className="my-6 md:px-10 lg:px-32">
+      <h2 className="text-2xl font-bold py-3">Profile</h2>
+      <UserProfile>
+        <UserButton.UserProfilePage
+          label="My Listing"
+          labelIcon={<Building className="h-5 w-5" />}
+          url="/dashboard/listing"
+        >
+          MY LISTING  
+        </UserButton.UserProfilePage>
+      </UserProfile>
+
+    </div>
+  );
 }
